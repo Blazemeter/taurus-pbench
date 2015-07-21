225280cb1a14f634d32e8f3f82b241c0c730edf3
Fedor Korotkiy <dartslon@gmail.com>
2015-07-21 17:14:53 +0300

    skip length

--- phantom/taurus_source/taurus_source.C
+++ phantom/taurus_source/taurus_source.C
@@ -33,130 +33,130 @@ struct schedule_t {
 	};
 
 	uint32_t type_and_delay_time_ms; // type in highest byte
-  uint32_t payload_len;
+	uint32_t payload_len;
 	uint64_t payload_start;
 };
 
 class taurus_source_t : public source_t {
 public:
-  struct config_t {
+	struct config_t {
 		string_t ammo, schedule;
-    interval_t max_test_duration;
+		interval_t max_test_duration;
 
-    config_t() : max_test_duration(interval::inf) {}
+		config_t() : max_test_duration(interval::inf) {}
 
 		void check(in_t::ptr_t const &ptr) const {
 			if(!ammo)
 				config::error(ptr, "ammo is required");
-      if(!schedule)
+			if(!schedule)
 				config::error(ptr, "schedule is required");
-    }
-  };
+		}
+	};
 
-  int fread_or_throw(void* ptr, size_t size) const {
-    int ret = fread(ptr, size, 1, schedule);
-    if(ret == 0 && ferror(schedule)) {
-      throw exception_sys_t(log::error, errno, "fread: %m");
-    }
-    return ret;
-  }
+	int fread_or_throw(void* ptr, size_t size) const {
+		int ret = fread(ptr, size, 1, schedule);
+		if(ret == 0 && ferror(schedule)) {
+			throw exception_sys_t(log::error, errno, "fread: %m");
+		}
+		return ret;
+	}
 
-  bool read_schedule(schedule_t* sched, interval_t* interval_sleep) const {
-    mutex_guard_t guard(mutex);
+	bool read_schedule(schedule_t* sched, interval_t* interval_sleep) const {
+		mutex_guard_t guard(mutex);
 
-    int ret = fread_or_throw(sched, sizeof(*sched));
-    if(ret == 0) {
-      if(loop_offset >= 0) {
+		int ret = fread_or_throw(sched, sizeof(*sched));
+		if(ret == 0) {
+			if(loop_offset >= 0) {
 				log_debug("seeking to loop start at %ld", loop_offset);
-        if(fseek(schedule, loop_offset, SEEK_SET) == -1)
-          throw exception_sys_t(log::error, errno, "fseek: %m");
-        fread_or_throw(sched, sizeof(*sched));
-      } else {
-        return false;
-      }
-    }
+				if(fseek(schedule, loop_offset, SEEK_SET) == -1)
+					throw exception_sys_t(log::error, errno, "fseek: %m");
+				fread_or_throw(sched, sizeof(*sched));
+			} else {
+				return false;
+			}
+		}
 
 		interval_t delay = interval::millisecond * (sched->type_and_delay_time_ms & 0xFFFFFF);
 		timeval_last += delay;
 		*interval_sleep = timeval_last - timeval::current();
 
 		int flag = (sched->type_and_delay_time_ms >> 24) & 0xFF;
-    switch(flag) {
-    case schedule_t::START_LOOP:
-      if((loop_offset = ftell(schedule)) == -1)
-        throw exception_sys_t(log::error, errno, "ftell: %m");
+		switch(flag) {
+		case schedule_t::START_LOOP:
+			if((loop_offset = ftell(schedule)) == -1)
+				throw exception_sys_t(log::error, errno, "ftell: %m");
 
-      loop_offset -= sizeof(*sched);
+			loop_offset -= sizeof(*sched);
 			log_error("start loop at %d", loop_offset);
+			break;
+		case schedule_t::STOP:
+			stop = true;
+			break;
+		default:
+			break;
+		}
 
-      break;
-    case schedule_t::STOP:
-      stop = true;
-      break;
-    default:
-      break;
-    }
+		return true;
+	}
 
-    return true;
-  }
+	virtual bool get_request(in_segment_t &request, in_segment_t &tag) const {
+		try {
+			schedule_t sched;
 
-  virtual bool get_request(in_segment_t &request, in_segment_t &tag) const {
-    try {
-      schedule_t sched;
-
-      if(stop) {
+			if(stop) {
 				log_debug("stop flag encountered, stopping");
 				return false;
 			}
-      if(timeval::current() > deadline) {
+			if(timeval::current() > deadline) {
 				log_debug("test duration exceeded, stopping");
 				return false;
 			}
 
 			interval_t interval_sleep = interval::zero;
-      if(!read_schedule(&sched, &interval_sleep)) {
+			if(!read_schedule(&sched, &interval_sleep)) {
 				log_error("end of file, stopping");
 				return false;
 			}
 
-      string_t::ctor_t ctor(sched.payload_len);
+			string_t::ctor_t ctor(sched.payload_len);
 			for(size_t i = 0; i < sched.payload_len; ++i) {
 				ctor(' ');
 			}
-      string_t payload(ctor);
+			string_t payload(ctor);
 
-      size_t size = 0;
-      while(size != sched.payload_len) {
+			size_t size = 0;
+			while(size != sched.payload_len) {
 				uint64_t offset = sched.payload_start + size;
-        int ret = pread(ammo_fd, (char*)payload.ptr() + size, sched.payload_len - size, offset);
-        if(ret == -1) {
-          throw exception_sys_t(log::error, errno, "pread(%p, %lu, %lu): %m",
-						(char*)payload.ptr() + size, sched.payload_len - size, offset);
-        }
+				int ret = pread(ammo_fd, (char*)payload.ptr() + size, sched.payload_len - size, offset);
+				if(ret == -1) {
+					throw exception_sys_t(log::error, errno, "pread(): %m");
+				}
 
-        size += ret;
-        if(ret == 0 && size != sched.payload_len) {
-          throw exception_log_t(log::error, "schedule out of range %ld %d", sched.payload_start, sched.payload_len);
-        }
-      }
+				size += ret;
+				if(ret == 0 && size != sched.payload_len) {
+					throw exception_log_t(log::error, "schedule payload out of range %ld %d", sched.payload_start, sched.payload_len);
+				}
+			}
 
-//			log_error("size: %ld", payload.size());
-
-      in_t::ptr_t start = payload;
-      in_t::ptr_t tagp = start;
-
+			in_t::ptr_t start = payload;
 			size_t limit = TAG_LEN;
-      if(!tagp.scan("\r\n", 2, limit))
-        throw exception_log_t(log::error, "format error #1");
+
+			in_t::ptr_t tagstart = start;
+			if(!tagstart.scan(" ", 1, limit))
+				throw exception_log_t(log::error, "format error #0");
+			tagstart++;
+
+			in_t::ptr_t tagp = tagstart;
+
+			if(!tagp.scan("\r\n", 2, limit))
+				throw exception_log_t(log::error, "format error #1");
 			tagp++; tagp++;
 
-      tag = in_segment_t(start, tagp - start - 2);
-//			log_error("tag %d %d", tag.off(), tag.size());
+			tag = in_segment_t(tagstart, tagp - tagstart - 2);
 
-      in_t::ptr_t end = start + (size_t)sched.payload_len;
+			in_t::ptr_t end = start + (size_t)sched.payload_len;
 
-      request = in_segment_t(tagp, end - tagp);
-//			log_error("request: %d %d", request.off(), request.size());
+			request = in_segment_t(tagp, end - tagp);
 
 			if(interval_sleep > interval::zero) {
 				if(bq_sleep(&interval_sleep) < 0) {
@@ -164,59 +164,59 @@ public:
 				}
 			}
 
-      return true;
-    } catch(...) {
-      stop = true;
-      throw;
-    }
-  }
+			return true;
+		} catch(...) {
+			stop = true;
+			throw;
+		}
+	}
 
-  virtual void do_init() {
-	  MKCSTR(_ammo_filename, ammo_filename);
-	  MKCSTR(_schedule_filename, schedule_filename);
+	virtual void do_init() {
+		MKCSTR(_ammo_filename, ammo_filename);
+		MKCSTR(_schedule_filename, schedule_filename);
 
-    ammo_fd = open(_ammo_filename, O_RDONLY, 0);
-    if(ammo_fd < 0)
-      throw exception_sys_t(log::error, errno, "open (%s): %m", _ammo_filename);
+		ammo_fd = open(_ammo_filename, O_RDONLY, 0);
+		if(ammo_fd < 0)
+			throw exception_sys_t(log::error, errno, "open (%s): %m", _ammo_filename);
 
-    schedule = fopen(_schedule_filename, "r");
-    if(!schedule) {
-      close(ammo_fd);
-      throw exception_sys_t(log::error, errno, "fopen (%s): %m", _schedule_filename);
-    }
-  }
+		schedule = fopen(_schedule_filename, "r");
+		if(!schedule) {
+			close(ammo_fd);
+			throw exception_sys_t(log::error, errno, "fopen (%s): %m", _schedule_filename);
+		}
+	}
 
-  virtual void do_run() const {}
-  virtual void do_stat_print() const {}
+	virtual void do_run() const {}
+	virtual void do_stat_print() const {}
 
-  virtual void do_fini() {
-    if(schedule) {
-      fclose(schedule);
-      schedule = NULL;
-    }
+	virtual void do_fini() {
+		if(schedule) {
+			fclose(schedule);
+			schedule = NULL;
+		}
 
-    if(ammo_fd != -1) {
-      close(ammo_fd);
-      ammo_fd = -1;
-    }
-  }
+		if(ammo_fd != -1) {
+			close(ammo_fd);
+			ammo_fd = -1;
+		}
+	}
 
-  taurus_source_t(string_t const &name, config_t const &config) :
-    source_t(name), ammo_fd(-1),
-    ammo_filename(config.ammo), schedule_filename(config.schedule),
-    deadline(timeval::current() + config.max_test_duration),
-    loop_offset(-1), stop(false),
+	taurus_source_t(string_t const &name, config_t const &config) :
+		source_t(name), ammo_fd(-1),
+		ammo_filename(config.ammo), schedule_filename(config.schedule),
+		deadline(timeval::current() + config.max_test_duration),
+		loop_offset(-1), stop(false),
 		timeval_last(timeval::current()) {}
 
 private:
-  mutex_t mutable mutex;
-  int ammo_fd;
-  FILE* schedule;
-  string_t ammo_filename, schedule_filename;
-  timeval_t deadline;
+	mutex_t mutable mutex;
+	int ammo_fd;
+	FILE* schedule;
+	string_t ammo_filename, schedule_filename;
+	timeval_t deadline;
 
-  mutable int64_t loop_offset;
-  mutable std::atomic<bool> stop;
+	mutable int64_t loop_offset;
+	mutable std::atomic<bool> stop;
 	mutable timeval_t timeval_last;
 };
 
--- test.py
+++ test.py
@@ -2,14 +2,137 @@ import os
 import sys
 import struct
 import subprocess
-import BaseHTTPServer
+import unittest
 
 TEST_AMMO = [
-    ("", "GET / HTTP/1.1\r\n\r\n"),
-    ("login", "GET /login/u1 HTTP/1.1\r\nHost: google.com\r\n\r\n"),
-    ("abc123", "GET /abc HTTP/1.1\r\n\r\n"),
-    ("abc123", "GET /123 HTTP/1.1\r\n\r\n"),
+    ("1234 f", "GET / HTTP/1.1\r\n\r\n"),
+    ("23 login", "GET /login/u1 HTTP/1.1\r\nHost: google.com\r\n\r\n"),
+    ("43 abc123", "GET /abc HTTP/1.1\r\n\r\n"),
+    ("1233 abc123", "GET /123 HTTP/1.1\r\n\r\n"),
 ]
+
+CONFIG_TEMPLATE="""
+setup_t module_setup = setup_module_t {
+    dir = "/usr/lib/phantom"
+    list = {
+        io_benchmark
+        io_benchmark_method_stream
+        io_benchmark_method_stream_ipv4
+        io_benchmark_method_stream_source_random
+        io_benchmark_method_stream_proto_http
+        io_stream
+        io_stream_ipv4
+        io_stream_proto_http
+        io_stream_proto_http_handler_null
+    }
+}
+
+setup_t module_setup = setup_module_t {
+    dir = "lib/phantom"
+    list = {
+        taurus_source
+    }
+}
+
+scheduler_t main_scheduler = scheduler_simple_t {
+    threads = 4
+}
+
+io_t stream_io = io_stream_ipv4_t {
+    proto_t http_proto = proto_http_t {
+        logger_t request_logger = logger_default_t {
+            filename = "request.log"
+            scheduler = main_scheduler
+        }
+
+        loggers = { request_logger }
+
+        handler_t null_handler = handler_null_t {}
+
+        host = {
+            "localhost": {
+                path = {
+                    "/": {
+                        handler = null_handler
+                    }
+                }
+            }
+        }
+    }
+
+    proto = http_proto
+
+    address = 127.0.0.1
+    port = 8080
+    listen_backlog = 1K
+
+    scheduler = main_scheduler
+}
+
+io_t benchmark_io = io_benchmark_t {
+    method_t stream_method = method_stream_ipv4_t {
+        source_t taurus_source = taurus_source_t {
+            ammo = "ammo.stpd"
+            schedule = "schedule.sch"
+            {source_extra_conf}
+        }
+
+        proto_t http_proto = proto_http_t { }
+
+        address = 127.0.0.1
+        port = 8080
+        timeout = 5s
+        source = taurus_source
+        proto = http_proto
+
+        logger_t default_logger = logger_default_t {
+            filename = "benchmark-default.log"
+            level = proto_warning
+            scheduler = main_scheduler
+        }
+
+        logger_t brief_logger = logger_brief_t {
+            filename = "benchmark-brief.log"
+            scheduler = main_scheduler
+        }
+
+        loggers = { default_logger brief_logger }
+    }
+
+    times_t simple_times = times_simple_t {
+        max = 3s
+        min = 10
+        steps = 20
+    }
+
+    instances = 1
+    method = stream_method
+    times = simple_times
+
+    scheduler = main_scheduler
+}
+"""
+
+PHANTOM_TEST_CONF="phantom_test.conf"
+
+class TaurusSourceTestCase(unittest.TestCase):
+    def create_config(self, **kwargs):
+        open(PHANTOM_TEST_CONF, "w").write(CONFIG_TEMPLATE.format(**kwargs))
+
+    def start_phantom(self):
+        self.phantom = subprocess.Popen(["phantom", "run", PHANTOM_TEST_CONF])
+
+    def parse_access_log(self):
+        log = []
+        for line in open("test_access.log"):
+            pass
+        return log
+
+    def test_without_loop(self):
+        pass
+
+
+
 
 def test_phantom():
     offsets = []
